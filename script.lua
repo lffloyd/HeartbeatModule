9d9ef3ddf4b7612eb28c60c6f4e78e70ac1be0cc
lffloyd <lfmatosmelo@midiacom.uff.br>
2018-08-06 23:38:47 -0300

    Add files via upload

--- script.lua
+++ script.lua
@@ -1,29 +1,12 @@
---Local variables.
-local rate = 0
-
-server = 'm11.cloudmqtt.com'
-user = 'ghpqajfd'
-password = 'LxnxmMcKgIYt'
-port = 17639
-
---Variaveis para uso da biblioteca client MQTT
-mqtt = require 'paho.mqtt'
-socket = require 'socket'
-client = mqtt.client.create(server, port, call)
-
-client:auth(user, password)
-client:connect('conn_eyre')
-client:subscribe({'oi'})
-
-function call(topico, mensagem)
-	print("Tópico: "..topico.."\tMsg.: "..mensagem)
+function call(topic, message)
+	print("Topic: "..topic.."\tMsg.: "..message)
 end
 
 function getHeartbeatRate()
-	--The instructions to get the rate will be here.
-	--return math.random(0, 100)
 	client:handler()
 	socket.sleep(1)
+	print(i)
+	i=i+1
 end
 
 --Makes the program wait for 'n' seconds.
@@ -32,7 +15,7 @@ function sleep(n)
   	repeat until os.clock() > ntime
 end
 
---Draw a given text on the screen.
+--Draws a text on the screen.
 function drawText(rate)
 	canvas:attrColor('black')
 	canvas:drawRect('fill', 0, 0, canvas:attrSize())
@@ -42,22 +25,24 @@ function drawText(rate)
 	canvas:flush()
 end
 
---Handles 'presentation' events.
+server = 'm11.cloudmqtt.com'
+user = 'ghpqajfd'
+password = 'LxnxmMcKgIYt'
+port = 17639
+
+--Variables for MQTT client use.
+mqtt = require 'paho.mqtt'
+socket = require 'socket'
+client = mqtt.client.create(server, port, call)
+
+client:auth(user, password)
+client:connect('conn_eyre')
+client:subscribe({'oi'})
+
+--'Handler' function.
 function handler(evt)
-	--if evt.class ~= 'ncl' then return end
-	--if evt.type ~= 'presentation' then return end
-	--if evt.action == 'start' then
-		--while true do
-			--sleep(1)
-			--rate = getHeartbeatRate()
-			--for i=1,2 do
-			 	getHeartbeatRate()
-			--end 
-			--print("\nHeartbeat rate: "..rate)
-			--drawText(rate)
-		--end
-	--end
+	i=1
+	while (true) do	getHeartbeatRate() end
 end
 
 event.register(handler)
---handler(nil)
